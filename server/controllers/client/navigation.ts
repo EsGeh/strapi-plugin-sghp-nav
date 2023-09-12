997bd15384c7622c987a34fbeea4d15b36e84e53
EsGeh <samuel@esgeh.net>
2023-09-12 15:51:46 +0200

    fix public rest if. stick to strapi response format.

--- server/controllers/client/navigation.ts
+++ server/controllers/client/navigation.ts
@@ -6,15 +6,26 @@ import * as utils from '@strapi/utils';
 export default factories.createCoreController('plugin::sghp-nav.navigation', ({ strapi }) =>  ({
 
   async render(ctx) {
-    const query = await this.sanitizeQuery( ctx );
-    try {
-      ctx.body = await strapi.service( 'plugin::sghp-nav.clientNav' ).render(
-        query
-      );
-    }
-    catch (err) {
-      ctx.throw(500, err);
-    }
+    await this.validateQuery(ctx);
+    const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+    const results = await strapi.service('plugin::sghp-nav.clientNav').render( sanitizedQueryParams );
+    const sanitizedResults = results;
+    return this.transformResponse( sanitizedResults );
+  },
+
+  /* also every standard function
+   * has to be overwritten to use
+   * the `clientNav` service
+   * (annoying)
+   */
+
+  async find(ctx) {
+    await this.validateQuery(ctx);
+    const sanitizedQueryParams = await this.sanitizeQuery(ctx);
+    console.log( JSON.stringify( sanitizedQueryParams ) );
+    const results = await strapi.service('plugin::sghp-nav.clientNav').find( sanitizedQueryParams );
+    const sanitizedResults = results;
+    return this.transformResponse(sanitizedResults);
   },
 
 } ));
